efb7c21cd43afd4ff55aaa23b9937500dcc99c05
Steve Kemsley <steve.kemsley@magicka.co.uk>
2021-02-02 18:14:41 +0000

    Added wpad.dat to make-wpad

--- make-wpad-server.sh
+++ make-wpad-server.sh
@@ -16,10 +16,39 @@ wget http://remote.magicka.co.uk/gohttp -O /usr/bin/gohttp
 echo " * Making wpad dir"
 mkdir /var/lib/tftpboot/www/wpad -p
 echo " * Making default wpad.dat"
-cat << EOF > /var/lib/tftpboot/www/wpad/wpad.dat
+cat << EOF > /var/lib/tftpboot/www/wpad.dat
+function FindProxyForURL(url, host) {
+        if(isPlainHostName(host)) {
+                return "DIRECT"
+        }
+        var resolved_ip = dnsResolve(host);
 
+        if (isInNet(resolved_ip, "10.0.0.0", "255.0.0.0") ||
+            isInNet(resolved_ip, "172.16.0.0",  "255.240.0.0") ||
+            isInNet(resolved_ip, "192.168.0.0", "255.255.0.0") ||
+            isInNet(resolved_ip, "127.0.0.0", "255.255.255.0"))
+            return "DIRECT";
 
+        return "PROXY proxy.swgfl.org.uk:8080";
+
+}
 EOF
+echo " * Making staff staff.dat"
+cat << EOF > /var/lib/tftpboot/www/wpad/staff.dat
+function FindProxyForURL(url, host) {
+        if(isPlainHostName(host)) {
+                return "DIRECT"
+        }
+        var resolved_ip = dnsResolve(host);
+
+        if (isInNet(resolved_ip, "10.0.0.0", "255.0.0.0") ||
+            isInNet(resolved_ip, "172.16.0.0",  "255.240.0.0") ||
+            isInNet(resolved_ip, "192.168.0.0", "255.255.0.0") ||
+            isInNet(resolved_ip, "127.0.0.0", "255.255.255.0"))
+            return "DIRECT";
+
+        return "PROXY nossl.proxy1.rmsafetynet.com:8080";
+}
 
 
 exit 0
