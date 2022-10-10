9c7106dd677b421b90ea276e58183a5e157bdf21
Steve Kemsley <steve.kemsley@magicka.co.uk>
2022-10-10 20:40:21 +0100

    Added install-senagent.sh

--- install-senagent.sh
+++ install-senagent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senagent_linux_amd64
-wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senupdater_linux_amd64
+wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senagent/senagent_linux_amd64
+wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senagent/senupdater_linux_amd64
 
 chnod 700 sen*
 mv sen* /usr/bin
