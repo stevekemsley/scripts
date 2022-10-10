48b83a1fdaa4305485238065d48bfa84f000527e
Steve Kemsley <steve.kemsley@magicka.co.uk>
2022-10-10 20:41:11 +0100

    fixed typo

--- install-senagent.sh
+++ install-senagent.sh
@@ -3,7 +3,7 @@
 wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senagent/senagent_linux_amd64
 wget https://www.mgka.net/www/orgs/magicka.co.uk/software/senagent/senupdater_linux_amd64
 
-chnod 700 sen*
+chmod 700 sen*
 mv sen* /usr/bin
 
 exit 0
